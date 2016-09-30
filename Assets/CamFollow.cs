0139b57d107e165a88630e6af905a8c2898fee90
Caleb Yarnell <cmyarnell@ou.edu>
2016-09-30 17:54:44 -0500

    Created crude Player script. Created Camera script.

---
+++ Assets/CamFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamFollow : MonoBehaviour {
+
+    // Use this for initialization
+    GameObject player;
+
+    void Start () {
+
+        
+
+        player = GameObject.FindWithTag("Player");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+    }
+}
