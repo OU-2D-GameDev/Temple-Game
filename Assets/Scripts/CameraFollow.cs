eede307cbad2d7b743ef75378e730a54a5e8e670
Tyler Hunt <tyty5949@gmail.com>
2016-10-01 16:34:31 -0500

    Setup project structure and fixed simple protag

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,21 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CamFollow : MonoBehaviour {
+public class CameraFollow : MonoBehaviour {
 
-    // Use this for initialization
-    GameObject player;
+    private GameObject player;
 
-    void Start () {
-
-        
-
+    void Start ()
+	{
         player = GameObject.FindWithTag("Player");
-	
 	}
-	
-	// This is for you!
-	void Update () {
+
+	void Update ()
+	{
         transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
     }
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed = 7;
+
+    private Rigidbody2D playerRigidBody;
+
+    void Start()
+    {
+		playerRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+		playerRigidBody.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, playerRigidBody.velocity.y);
+    }
+}
